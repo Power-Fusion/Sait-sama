672c4767640eda8d7aa6b3d06e44f8060fb32d37
Power-Fusion <Power-Fusion@users.noreply.github.co>
2015-01-28 15:32:45 -0700

    .
    .

--- Sait-sama.meta.js
+++ Sait-sama.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Sait-sama
-// @version     2.1.6b
+// @version     2.1.7
 // @namespace   Power-Fusion
 // @description Sait Login Weab
 // @match       https://learn.sait.ca/
